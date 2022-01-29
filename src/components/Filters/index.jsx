e45a7e05444159fc772be53602fa87b78996c335
Matheus Abreu <theus.santosab@gmail.com>
2022-01-28 22:36:27 -0300

    finalizando a estilização de filtrar por região

--- src/components/Filters/index.jsx
+++ src/components/Filters/index.jsx
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
 
 import "./filters.css";
 
+const options = [
+  {
+    name: "teste1",
+    id: 1,
+  },
+  {
+    name: "teste2",
+    id: 2,
+  },
+  {
+    name: "teste3",
+    id: 3,
+  },
+];
+
 export const Filters = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState("");
+
+  const toggling = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const onOptionClicked = (value) => {
+    const option = value.target.innerHTML;
+    setSelectedOption(option);
+    setIsOpen(false);
+    console.log(selectedOption);
+  };
+
   return (
     <div className="Container">
       <div className="filters">
@@ -16,11 +45,30 @@ export const Filters = () => {
           />
         </div>
 
-        <select className="filterByRegion">
-          <option value="" className="optionRegion">
-            teste
-          </option>
-        </select>
+        <div className="select-box">
+          <div className="selected" onClick={toggling}>
+            {selectedOption || "Mongoes"}
+          </div>
+          {isOpen && (
+            <div className="select-items-container">
+              <div className="items">
+                {options.map((option) => (
+                  <div className="listItem">
+                    <input
+                      type="radio"
+                      className="listItem"
+                      key={option.id}
+                      id={option.id}
+                    />
+                    <label htmlFor={option.id} onClick={onOptionClicked}>
+                      {option.name}
+                    </label>
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
